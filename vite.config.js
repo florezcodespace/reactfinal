aa9a78401013f6ab931faf6da5f226d48a2cca7f
Andrés Florez <afflorez06@gmail.com>
2026-04-15 11:55:56 -0500

    Adjust maskable icon purpose

--- vite.config.js
+++ vite.config.js
@@ -69,7 +69,7 @@ export default defineConfig(({ command }) => ({
             src: '/pwa-maskable-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'maskable',
           },
         ],
       },
